0a421dfc97ec8d0a06c3a090f65b57fbaa9156b4
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-27 22:42:49 +0200

    Add health script

--- Assets/Scripts/ShotScript.cs
+++ Assets/Scripts/ShotScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShotScript : MonoBehaviour
+public class ShotScript : MonoBehaviour
 {
     public int damage = 1;
     public bool isEnemyShot = false;
