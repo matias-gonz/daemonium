bb6dc20fd573bddf8f235b470b69fb45f1d55089
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-27 22:38:52 +0200

    Add ShotMoveScript

---
+++ Assets/Scripts/ShotMoveScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShotMoveScript : MonoBehaviour
+{
+    public Vector2 speed = new Vector2(10, 10);
+    public Vector2 direction = new Vector2(1, 0);
+    public Vector2 movement;
+    private Rigidbody2D _rigidbody2D;
+    
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
+    
+    void FixedUpdate()
+    {
+        movement = new Vector2(
+            speed.x * direction.x,
+            speed.y * direction.y
+        );
+        _rigidbody2D.velocity = movement;
+    }
+}
