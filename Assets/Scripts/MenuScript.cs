7df81cf936eda8a787406c27e7879e53fa86062c
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-29 00:23:00 +0200

    Add MenuScript

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Application.LoadLevel("Main");
+    }
+}
