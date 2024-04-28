1ed4c96d321fef7312f58adf4c059637001824e4
matias-gonz <maigonzalez@fi.uba.ar>
2024-04-29 01:45:18 +0200

    Update Menu

--- Assets/Scripts/MenuScript.cs
+++ Assets/Scripts/MenuScript.cs
@@ -4,6 +4,18 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+
+    public GameObject mainMenu;
+    public GameObject characterSelectionMenu;
+    public GameObject multiplayerMenu;
+    
+    private void Start()
+    {
+        mainMenu.gameObject.SetActive(true);
+        characterSelectionMenu.gameObject.SetActive(false);
+        multiplayerMenu.gameObject.SetActive(false);
+    }
+    
     private void StartGame()
     {
         SceneManager.LoadScene("Main");
@@ -13,12 +25,27 @@ public class MenuScript : MonoBehaviour
     {
         ApplicationScript.Gamemode = ApplicationScript.GamemodeEnum.SinglePlayer;
         ApplicationScript.Character = ApplicationScript.CharacterEnum.Morgana;
-        StartGame();
+        mainMenu.gameObject.SetActive(false);
+        characterSelectionMenu.gameObject.SetActive(true);
     }
     
     public void PressMultiPlayer()
     {
         ApplicationScript.Gamemode = ApplicationScript.GamemodeEnum.MultiPlayer;
+        mainMenu.gameObject.SetActive(false);
+        multiplayerMenu.gameObject.SetActive(true);
+        Invoke(nameof(StartGame), 1);
+    }
+    
+    public void PressCirce()
+    {
+        ApplicationScript.Character = ApplicationScript.CharacterEnum.Circe;
+        StartGame();
+    }
+    
+    public void PressMorgana()
+    {
+        ApplicationScript.Character = ApplicationScript.CharacterEnum.Morgana;
         StartGame();
     }
 }
